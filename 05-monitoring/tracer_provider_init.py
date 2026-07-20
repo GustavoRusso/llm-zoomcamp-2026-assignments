ededac3844f74cd78aebb9f2ab8fc834573529ad
Gustavo Russo <GustavoRusso@users.noreply.github.com>
2026-07-20 21:36:39 +0000

    completed hw5

--- 05-monitoring/tracer_provider_init.py
+++ 05-monitoring/tracer_provider_init.py
@@ -1,8 +1,10 @@
 from opentelemetry import trace
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
+from sqlite_span_exporter import SQLiteSpanExporter
 
 def setup_tracing():
     provider = TracerProvider()
-    provider.add_span_processor(SimpleSpanProcessor(ConsoleSpanExporter()))
+    #provider.add_span_processor(SimpleSpanProcessor(ConsoleSpanExporter()))
+    provider.add_span_processor(SimpleSpanProcessor(SQLiteSpanExporter("traces.db")))
     trace.set_tracer_provider(provider)
